efb6cdd32259a31546cef68f6ac430d65a0f6959
Mateus Gomes <monkmitogames@hotmail.com>
2024-07-01 20:00:57 -0300

    Mais informações na sessões sobre mim

--- src/components/WelcomeSection.js
+++ src/components/WelcomeSection.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import perfilImage from '../fotos/perfil.jpg';
-//import './WelcomeSection.css';
+import '../estilos/WelcomeSection.scss';
 
 function WelcomeSection() {
   return (
     <section id="welcome-section">
-    <img src={perfilImage} alt="Foto de quem eu sou" id="my-photo" />
-    <h1>Bem-vindo ao meu Portfólio</h1>
-  </section>
+      <img src={perfilImage} alt="Uma foto mostrando quem eu sou" id="my-photo" />
+      <h1>Bem-vindo ao meu Portfólio</h1>
+      <p>
+        Olá meu nome é Mateus mas meus amigos só me chamam de Gomes, Tudo bem?! Sou formado em Ciência da Computação pela UNICAP. Atualmente, estou me aprofundando em marketing digital com Google Ads, Node.js, Express e MongoDB. Tenho experiência em Java focada em aplicativos de console, além de um forte conhecimento em front-end com HTML, CSS, JavaScript e React. Desenvolvi uma API para um sistema de almoxarifado durante meu tempo na empresa júnior da faculdade.
+      </p>
+      <p>
+        Adoro aprender e estou sempre em busca de novas oportunidades. Sou criativo na resolução de problemas e gosto de colaborar para o crescimento da equipe. Acredito que um problema em um projeto é um desafio para todos.
+      </p>
+    </section>
   );
 }
 
